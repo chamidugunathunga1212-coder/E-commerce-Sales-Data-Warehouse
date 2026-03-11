76e24e3b7e79f6881f841c0da020a7bcf14a357e
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-11 12:54:29 +0530

    Add usp_load_orders and usp_load_order_items procedures
    Create or alter procedures to load orders and order items into the silver schema.

--- scripts/silver/create_procedures.sql
+++ scripts/silver/create_procedures.sql
@@ -1,4 +1,45 @@
 
+-- create silver.usp_load_orders procedure
+
+CREATE OR ALTER PROCEDURE silver.usp_load_orders
+AS
+    BEGIN
+
+        TRUNCATE TABLE silver.orders;
+
+        INSERT INTO silver.orders (
+            order_id,
+            customer_id,
+            order_status,
+            order_purchase_timestamp,
+            order_approved_at,
+            order_delivered_carrier_date,
+            order_delivered_customer_date,
+            order_estimated_delivery_date
+        )
+        SELECT 
+	        TRIM(REPLACE(order_id,'"','')) AS order_id,
+            TRIM(REPLACE(customer_id,'"','')) AS customer_id,
+            LOWER(TRIM(order_status)) AS order_status,
+            TRY_CAST(order_purchase_timestamp AS DATETIME2) AS order_purchase_timestamp,
+            TRY_CAST(order_approved_at AS DATETIME2) AS order_approved_at,
+            TRY_CAST(order_delivered_carrier_date AS DATETIME2) AS order_delivered_carrier_date,
+            TRY_CAST(order_delivered_customer_date AS DATETIME2) AS order_delivered_customer_date,
+            TRY_CAST(order_estimated_delivery_date AS DATETIME2) AS order_estimated_delivery_date
+
+        FROM bronze.orders;
+    END;
+GO
+
+
+
+
+
+
+
+-- create silver.usp_load_order_items procedure
+
+
 CREATE OR ALTER PROCEDURE silver.usp_load_order_items
 AS
     BEGIN
