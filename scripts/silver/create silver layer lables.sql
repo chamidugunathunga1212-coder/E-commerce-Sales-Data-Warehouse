7ed41af912950af4d6ce490e3b3b020a4a5e6d9d
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-11 14:28:35 +0530

    Create tables for silver layer in SQL script

--- scripts/silver/create silver layer lables.sql
+++ scripts/silver/create silver layer lables.sql
@@ -38,3 +38,54 @@ CREATE TABLE silver.customers (
 );
 GO
 
+
+CREATE TABLE silver.sellers (
+    seller_id VARCHAR(50) NOT NULL,
+    seller_zip_code_prefix INT NULL,
+    seller_city VARCHAR(100) NULL,
+    seller_state CHAR(2) NULL
+);
+GO
+
+CREATE TABLE silver.products (
+    product_id VARCHAR(50) NOT NULL,
+    product_category_name VARCHAR(100) NULL,
+    product_category_name_english VARCHAR(100) NULL,
+    product_name_length INT NULL,
+    product_description_length INT NULL,
+    product_photos_qty INT NULL,
+    product_weight_g INT NULL,
+    product_length_cm DECIMAL(10,2) NULL,
+    product_height_cm DECIMAL(10,2) NULL,
+    product_width_cm DECIMAL(10,2) NULL
+);
+GO
+
+CREATE TABLE silver.payments (
+    order_id VARCHAR(50) NOT NULL,
+    payment_sequential INT NOT NULL,
+    payment_type VARCHAR(30) NULL,
+    payment_installments INT NULL,
+    payment_value DECIMAL(10,2) NULL
+);
+
+CREATE TABLE silver.reviews (
+    review_id VARCHAR(50) NOT NULL,
+    order_id VARCHAR(50) NOT NULL,
+    review_score INT NULL,
+    review_comment_title NVARCHAR(255) NULL,
+    review_comment_message NVARCHAR(MAX) NULL,
+    review_creation_date DATE NULL,
+    review_answer_timestamp DATETIME2 NULL
+);
+GO
+
+CREATE TABLE silver.geolocation (
+    geolocation_zip_code_prefix INT NOT NULL,
+    geolocation_city VARCHAR(100) NULL,
+    geolocation_state CHAR(2) NULL,
+    geolocation_lat DECIMAL(10,6) NULL,
+    geolocation_lng DECIMAL(10,6) NULL
+);
+GO
+
