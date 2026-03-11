cd761991cb9cb628ce069a74bc409b19e1c1d170
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-11 13:02:48 +0530

    Modify usp_load_order_items to clean input fields
    Updated usp_load_order_items procedure to replace double quotes in order_id, product_id, and seller_id fields.

--- scripts/silver/create_procedures.sql
+++ scripts/silver/create_procedures.sql
@@ -39,6 +39,8 @@ GO
 
 -- create silver.usp_load_order_items procedure
 
+-- create silver.usp_load_order_items procedure
+
 
 CREATE OR ALTER PROCEDURE silver.usp_load_order_items
 AS
@@ -56,17 +58,18 @@ AS
             freight_value
         )
         SELECT 
-	        TRIM(order_id) AS order_id,
+	        TRIM(REPLACE(order_id,'"','')) AS order_id,
             TRY_CAST(order_item_id AS INT) AS order_item_id,
-            TRIM(product_id) AS product_id,
-            TRIM(seller_id) AS seller_id,
+            TRIM(REPLACE(product_id,'"','')) AS product_id,
+            TRIM(REPLACE(seller_id,'"','')) AS seller_id,
             TRY_CAST(shipping_limit_date AS DATETIME2) AS shipping_limit_date,
             TRY_CAST(price AS DECIMAL(10,2)) AS price,
             TRY_CAST(freight_value AS DECIMAL(10,2)) AS freight_value
         FROM bronze.order_items;
     END;
 GO
-
+	        
+            
 -- create silver.usp_load_customers procedure
 
 CREATE OR ALTER PROCEDURE silver.usp_load_customers
