7bb027db4d980aa5bf96aa40a564c70f3487066d
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-12 17:39:41 +0530

    Create insert Unkown rows.sql

---
+++ scripts/gold/insert Unkown rows.sql
@@ -0,0 +1,76 @@
+SET IDENTITY_INSERT gold.dim_customer ON;
+
+INSERT INTO gold.dim_customer (
+    customer_key,
+    customer_id,
+    customer_unique_id,
+    customer_zip_code_prefix,
+    customer_city,
+    customer_state
+)
+VALUES (0, 'UNKNOWN', 'UNKNOWN', 0, 'UNKNOWN', 'UN');
+
+SET IDENTITY_INSERT gold.dim_customer OFF;
+GO
+
+
+
+
+
+SET IDENTITY_INSERT gold.dim_product ON;
+INSERT INTO gold.dim_product (
+    product_key,
+    product_id,
+    product_category_name,
+    product_category_name_english,
+    product_name_length,
+    product_description_length,
+    product_photos_qty,
+    product_weight_g,
+    product_length_cm,
+    product_height_cm,
+    product_width_cm
+)
+VALUES (0, 'UNKNOWN', 'UNKNOWN', 0, 0, 0,0,0,0,0,0);
+
+SET IDENTITY_INSERT gold.dim_product OFF;
+GO
+
+
+
+
+
+
+
+SET IDENTITY_INSERT gold.dim_seller ON;
+INSERT INTO gold.dim_seller (
+    seller_key,
+    seller_id,
+    seller_zip_code_prefix,
+    seller_city,
+    seller_state
+)
+VALUES (0, 'UNKNOWN', 0, 'UNKNOWN','UN');
+
+SET IDENTITY_INSERT gold.dim_seller OFF;
+GO
+
+
+
+
+
+
+
+
+
+
+
+SELECT *
+FROM gold.dim_customer;
+
+SELECT *
+FROM gold.dim_product;
+
+
+SELECT *
+FROM gold.dim_seller;
