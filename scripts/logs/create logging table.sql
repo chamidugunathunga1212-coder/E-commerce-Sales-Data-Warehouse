7ddce673b98548b1b7bb377de020de993a4e9da1
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-20 00:25:12 +0530

    Update create logging table.sql

--- scripts/logs/create logging table.sql
+++ scripts/logs/create logging table.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE etl.etl_logs (
+    log_id INT IDENTITY(1,1) PRIMARY KEY,
+    process_name VARCHAR(100),
+    layer VARCHAR(20), -- Bronze / Silver / Gold
+    status VARCHAR(20), -- START / SUCCESS / FAILED
+    rows_processed INT NULL,
+    error_message VARCHAR(MAX) NULL,
+    log_time DATETIME DEFAULT GETDATE()
+);
