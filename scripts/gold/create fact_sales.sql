90100667fe4a81529cbf2b245be1fea921ffc653
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-20 22:30:38 +0530

    Update create fact_sales.sql

--- scripts/gold/create fact_sales.sql
+++ scripts/gold/create fact_sales.sql
@@ -2,16 +2,28 @@
 
 CREATE TABLE gold.fact_sales (
     sales_key BIGINT IDENTITY(1,1) PRIMARY KEY,
+
+    -- Business Keys
     order_id VARCHAR(50) NOT NULL,
     order_item_id INT NOT NULL,
+
+    -- Foreign Keys
     customer_key INT NOT NULL,
     seller_key INT NOT NULL,
     product_key INT NOT NULL,
     order_date_key INT NOT NULL,
+
+    -- Measures
     price DECIMAL(10,2) NOT NULL,
     freight_value DECIMAL(10,2) NOT NULL,
-    total_sales_amount DECIMAL(10,2) NOT NULL,
-    delivery_days INT NULL
+    total_sales_amount AS (price + freight_value), -- computed column
+
+    -- Derived Metrics
+    delivery_days INT NULL,
+
+    -- Additional Info
+    order_status VARCHAR(50),
+    order_purchase_timestamp DATETIME2,
+    order_delivered_customer_date DATETIME2
 );
 GO
-
