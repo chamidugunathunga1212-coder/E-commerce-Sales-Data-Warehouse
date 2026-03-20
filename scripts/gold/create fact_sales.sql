13921ae7aef1e76ce11d43b8022d3a567b6a34ca
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-20 22:33:45 +0530

    Update create fact_sales.sql

--- scripts/gold/create fact_sales.sql
+++ scripts/gold/create fact_sales.sql
@@ -27,3 +27,108 @@ CREATE TABLE gold.fact_sales (
     order_delivered_customer_date DATETIME2
 );
 GO
+
+
+
+
+-- sales facts procedure 
+    
+CREATE OR ALTER PROCEDURE gold.usp_load_fact_sales
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @row_count INT;
+
+    BEGIN TRY
+
+        -- 🔹 START LOG
+        INSERT INTO etl.etl_logs (process_name, layer, status)
+        VALUES ('usp_load_fact_sales', 'Gold', 'START');
+
+        -- 🔹 LOAD FACT TABLE
+        INSERT INTO gold.fact_sales (
+            order_id,
+            order_item_id,
+            customer_key,
+            seller_key,
+            product_key,
+            order_date_key,
+            price,
+            freight_value,
+            delivery_days,
+            order_status,
+            order_purchase_timestamp,
+            order_delivered_customer_date
+        )
+        SELECT 
+            oi.order_id,
+            oi.order_item_id,
+
+            c.customer_key,
+            s.seller_key,
+            p.product_key,
+
+            CONVERT(INT, CONVERT(CHAR(8), o.order_purchase_timestamp, 112)),
+
+            oi.price,
+            oi.freight_value,
+
+            -- 🔹 delivery_days calculation
+            CASE 
+                WHEN o.order_delivered_customer_date IS NOT NULL 
+                     AND o.order_purchase_timestamp IS NOT NULL
+                THEN DATEDIFF(
+                        DAY,
+                        o.order_purchase_timestamp,
+                        o.order_delivered_customer_date
+                     )
+                ELSE NULL
+            END AS delivery_days,
+
+            o.order_status,
+            o.order_purchase_timestamp,
+            o.order_delivered_customer_date
+
+        FROM silver.order_items oi
+
+        LEFT JOIN silver.orders o
+            ON oi.order_id = o.order_id
+
+        LEFT JOIN gold.dim_customer c
+            ON o.customer_id = c.customer_id
+
+        LEFT JOIN gold.dim_seller s
+            ON oi.seller_id = s.seller_id
+
+        LEFT JOIN gold.dim_product p
+            ON oi.product_id = p.product_id
+
+        WHERE oi.order_id IS NOT NULL;
+
+        SET @row_count = @@ROWCOUNT;
+
+        -- 🔹 SUCCESS LOG
+        INSERT INTO etl.etl_logs (
+            process_name, layer, status, rows_processed
+        )
+        VALUES (
+            'usp_load_fact_sales', 'Gold', 'SUCCESS', @row_count
+        );
+
+    END TRY
+    BEGIN CATCH
+
+        -- 🔹 ERROR LOG
+        INSERT INTO etl.etl_logs (
+            process_name, layer, status, error_message
+        )
+        VALUES (
+            'usp_load_fact_sales', 'Gold', 'FAILED', ERROR_MESSAGE()
+        );
+
+        THROW;
+
+    END CATCH
+END;
+GO
