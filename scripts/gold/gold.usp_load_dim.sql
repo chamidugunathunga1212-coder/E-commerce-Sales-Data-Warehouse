14da2e50b3bdc9ba53c5aa434f42019554b0c4d6
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-20 20:42:55 +0530

    Update gold.usp_load_dim.sql

--- scripts/gold/gold.usp_load_dim.sql
+++ scripts/gold/gold.usp_load_dim.sql
@@ -140,78 +140,6 @@ END;
 
 
 
--- gold.usp_load_product
-
-CREATE PROCEDURE gold.usp_load_dim_product
-AS
-BEGIN
-
-    MERGE gold.dim_product AS target
-    USING (
-        SELECT DISTINCT
-            product_id,
-            TRIM(product_category_name) AS product_category_name,
-            TRIM(product_category_name_english) AS product_category_name_english,
-            TRY_CAST(product_name_length AS INT) AS product_name_length,
-            TRY_CAST(product_description_length AS INT) AS product_description_length,
-            TRY_CAST(product_photos_qty AS INT) AS product_photos_qty,
-            TRY_CAST(product_weight_g AS INT) AS product_weight_g,
-            TRY_CAST(product_length_cm AS INT) AS product_length_cm,
-            TRY_CAST(product_height_cm AS INT) AS product_height_cm,
-            TRY_CAST(product_width_cm AS INT) AS product_width_cm
-        FROM silver.products
-        WHERE product_id IS NOT NULL
-    ) AS source
-    ON target.product_id = source.product_id
-
-    -- UPDATE existing
-    WHEN MATCHED THEN
-        UPDATE SET
-            target.product_category_name = source.product_category_name,
-            target.product_category_name_english = source.product_category_name_english,
-            target.product_name_length = source.product_name_length,
-            target.product_description_length = source.product_description_length,
-            target.product_photos_qty = source.product_photos_qty,
-            target.product_weight_g = source.product_weight_g,
-            target.product_length_cm = source.product_length_cm,
-            target.product_height_cm = source.product_height_cm,
-            target.product_width_cm = source.product_width_cm,
-            target.updated_date = GETDATE()
-
-    -- INSERT new
-    WHEN NOT MATCHED THEN
-        INSERT (
-            product_id,
-            product_category_name,
-            product_category_name_english,
-            product_name_length,
-            product_description_length,
-            product_photos_qty,a
-            product_weight_g,
-            product_length_cm,
-            product_height_cm,
-            product_width_cm,
-            created_date,
-            updated_date
-        )
-        VALUES (
-            source.product_id,
-            source.product_category_name,
-            source.product_category_name_english,
-            source.product_name_length,
-            source.product_description_length,
-            source.product_photos_qty,
-            source.product_weight_g,
-            source.product_length_cm,
-            source.product_height_cm,
-            source.product_width_cm,
-            GETDATE(),
-            GETDATE()
-        );
-
-END;
-
-
 
 
 
@@ -326,6 +254,129 @@ BEGIN
     END CATCH
 END;
 GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- gold.usp_load_dim_product
+
+CREATE OR ALTER PROCEDURE gold.usp_load_dim_product
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @row_count INT;
+
+    BEGIN TRY
+
+        -- START LOG
+        INSERT INTO etl.etl_logs (process_name, layer, status)
+        VALUES ('usp_load_dim_product', 'Gold', 'START');
+
+        -- MERGE (UPSERT)
+        MERGE gold.dim_product AS target
+        USING (
+            SELECT DISTINCT
+                product_id,
+                product_category_name,
+                product_category_name_english,
+                product_name_length,
+                product_description_length,
+                product_photos_qty,
+                product_weight_g,
+                product_length_cm,
+                product_height_cm,
+                product_width_cm
+            FROM silver.products
+            WHERE product_id IS NOT NULL
+        ) AS source
+        ON target.product_id = source.product_id
+
+        WHEN MATCHED THEN
+            UPDATE SET
+                target.product_category_name = source.product_category_name,
+                target.product_category_name_english = source.product_category_name_english,
+                target.product_name_length = source.product_name_length,
+                target.product_description_length = source.product_description_length,
+                target.product_photos_qty = source.product_photos_qty,
+                target.product_weight_g = source.product_weight_g,
+                target.product_length_cm = source.product_length_cm,
+                target.product_height_cm = source.product_height_cm,
+                target.product_width_cm = source.product_width_cm,
+                target.updated_date = GETDATE()
+
+        WHEN NOT MATCHED THEN
+            INSERT (
+                product_id,
+                product_category_name,
+                product_category_name_english,
+                product_name_length,
+                product_description_length,
+                product_photos_qty,
+                product_weight_g,
+                product_length_cm,
+                product_height_cm,
+                product_width_cm,
+                created_date,
+                updated_date
+            )
+            VALUES (
+                source.product_id,
+                source.product_category_name,
+                source.product_category_name_english,
+                source.product_name_length,
+                source.product_description_length,
+                source.product_photos_qty,
+                source.product_weight_g,
+                source.product_length_cm,
+                source.product_height_cm,
+                source.product_width_cm,
+                GETDATE(),
+                GETDATE()
+            );
+
+        SET @row_count = @@ROWCOUNT;
+
+        -- SUCCESS LOG
+        INSERT INTO etl.etl_logs (
+            process_name, layer, status, rows_processed
+        )
+        VALUES (
+            'usp_load_dim_product', 'Gold', 'SUCCESS', @row_count
+        );
+
+    END TRY
+    BEGIN CATCH
+
+        -- ERROR LOG
+        INSERT INTO etl.etl_logs (
+            process_name, layer, status, error_message
+        )
+        VALUES (
+            'usp_load_dim_product', 'Gold', 'FAILED', ERROR_MESSAGE()
+        );
+
+        THROW;
+
+    END CATCH
+END;
+GO
+
+
+
 
 
 
