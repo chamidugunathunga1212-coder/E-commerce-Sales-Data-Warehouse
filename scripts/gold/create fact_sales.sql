26932ff4cfd006005b909d07152f7a815ffb11d2
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-22 12:48:59 +0530

    Update create fact_sales.sql

--- scripts/gold/create fact_sales.sql
+++ scripts/gold/create fact_sales.sql
@@ -32,7 +32,6 @@ GO
 
 
 -- sales facts procedure 
-    
 CREATE OR ALTER PROCEDURE gold.usp_load_fact_sales
 AS
 BEGIN
@@ -46,65 +45,87 @@ BEGIN
         INSERT INTO etl.etl_logs (process_name, layer, status)
         VALUES ('usp_load_fact_sales', 'Gold', 'START');
 
-        -- LOAD FACT TABLE
-        INSERT INTO gold.fact_sales (
-            order_id,
-            order_item_id,
-            customer_key,
-            seller_key,
-            product_key,
-            order_date_key,
-            price,
-            freight_value,
-            delivery_days,
-            order_status,
-            order_purchase_timestamp,
-            order_delivered_customer_date
-        )
-        SELECT 
-            oi.order_id,
-            oi.order_item_id,
+        -- MERGE (INSERT ONLY NEW RECORDS)
+        MERGE gold.fact_sales AS target
+        USING (
+            SELECT 
+                oi.order_id,
+                oi.order_item_id,
 
-            c.customer_key,
-            s.seller_key,
-            p.product_key,
+                c.customer_key,
+                s.seller_key,
+                p.product_key,
 
-            CONVERT(INT, CONVERT(CHAR(8), o.order_purchase_timestamp, 112)),
+                CONVERT(INT, CONVERT(CHAR(8), o.order_purchase_timestamp, 112)) AS order_date_key,
 
-            oi.price,
-            oi.freight_value,
+                oi.price,
+                oi.freight_value,
 
-            -- delivery_days calculation
-            CASE 
-                WHEN o.order_delivered_customer_date IS NOT NULL 
-                     AND o.order_purchase_timestamp IS NOT NULL
-                THEN DATEDIFF(
-                        DAY,
-                        o.order_purchase_timestamp,
-                        o.order_delivered_customer_date
-                     )
-                ELSE NULL
-            END AS delivery_days,
+                CASE 
+                    WHEN o.order_delivered_customer_date IS NOT NULL 
+                         AND o.order_purchase_timestamp IS NOT NULL
+                    THEN DATEDIFF(
+                            DAY,
+                            o.order_purchase_timestamp,
+                            o.order_delivered_customer_date
+                         )
+                    ELSE NULL
+                END AS delivery_days,
 
-            o.order_status,
-            o.order_purchase_timestamp,
-            o.order_delivered_customer_date
+                o.order_status,
+                o.order_purchase_timestamp,
+                o.order_delivered_customer_date
 
-        FROM silver.order_items oi
+            FROM silver.order_items oi
 
-        LEFT JOIN silver.orders o
-            ON oi.order_id = o.order_id
+            LEFT JOIN silver.orders o
+                ON oi.order_id = o.order_id
 
-        LEFT JOIN gold.dim_customer c
-            ON o.customer_id = c.customer_id
+            LEFT JOIN gold.dim_customer c
+                ON o.customer_id = c.customer_id
 
-        LEFT JOIN gold.dim_seller s
-            ON oi.seller_id = s.seller_id
+            LEFT JOIN gold.dim_seller s
+                ON oi.seller_id = s.seller_id
 
-        LEFT JOIN gold.dim_product p
-            ON oi.product_id = p.product_id
+            LEFT JOIN gold.dim_product p
+                ON oi.product_id = p.product_id
 
-        WHERE oi.order_id IS NOT NULL;
+            WHERE oi.order_id IS NOT NULL
+
+        ) AS source
+
+        ON target.order_id = source.order_id
+        AND target.order_item_id = source.order_item_id
+
+        WHEN NOT MATCHED THEN
+            INSERT (
+                order_id,
+                order_item_id,
+                customer_key,
+                seller_key,
+                product_key,
+                order_date_key,
+                price,
+                freight_value,
+                delivery_days,
+                order_status,
+                order_purchase_timestamp,
+                order_delivered_customer_date
+            )
+            VALUES (
+                source.order_id,
+                source.order_item_id,
+                source.customer_key,
+                source.seller_key,
+                source.product_key,
+                source.order_date_key,
+                source.price,
+                source.freight_value,
+                source.delivery_days,
+                source.order_status,
+                source.order_purchase_timestamp,
+                source.order_delivered_customer_date
+            );
 
         SET @row_count = @@ROWCOUNT;
 
