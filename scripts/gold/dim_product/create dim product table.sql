5172598fc9238207fc0d3e78cc154be71d7228c1
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-23 22:27:16 +0530

    Update create dim product table.sql

--- scripts/gold/dim_product/create dim product table.sql
+++ scripts/gold/dim_product/create dim product table.sql
@@ -1,1 +1,19 @@
+CREATE TABLE gold.dim_product (
+    product_key INT IDENTITY(1,1) PRIMARY KEY,
+    product_id VARCHAR(50) NOT NULL,
+    product_category_name VARCHAR(100),
+    product_category_name_english VARCHAR(100),
+    product_name_length INT,
+    product_description_length INT,
+    product_photos_qty INT,
+    product_weight_g INT,
+    product_length_cm DECIMAL(10,2),
+    product_height_cm DECIMAL(10,2),
+    product_width_cm DECIMAL(10,2),
+
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NULL,
+    is_current BIT NOT NULL
+);
+
 
