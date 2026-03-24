2de38b287698fcb6aabb3097a9ef073d3c5dabf5
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-03-24 10:40:36 +0530

    Update create fact_order_accumulating.sql

--- scripts/gold/fact_order_accumulating/create fact_order_accumulating.sql
+++ scripts/gold/fact_order_accumulating/create fact_order_accumulating.sql
@@ -4,10 +4,10 @@ CREATE TABLE gold.fact_order_accumulating (
     customer_key INT,
     seller_key INT,
 
-    accm_txn_create_time DATETIME2,
-    accm_txn_complete_time DATETIME2,
+    create_time DATETIME2,
+    complete_time DATETIME2,
 
-    txn_process_time_hours INT,
+    process_hours INT,
 
     order_status VARCHAR(30)
 );
